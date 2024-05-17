d92a5f83844d50b67033fc5dbfd2e16b07f75ccb
Léon Anthony Menge <leonanthony.menge@decathlon.com>
2024-05-17 18:26:56 +0200

    Chat UI

--- FE/streamlit_app.py
+++ FE/streamlit_app.py
@@ -1,14 +1,55 @@
 import json
+import random
+import time
+
 import requests
 import streamlit as st
 import streamlit_authenticator as stauth
 import asyncio
 from streamlit_signup_test import sign_up, get_users
 
+base_url = 'http://localhost:4000'
+if 'messages' not in st.session_state:
+    st.session_state.messages = []
+
+if 'selected_thread_id' not in st.session_state:
+    st.session_state.selected_thread_id = None
+
 
 async def get_threads():
-    res = requests.get(f"http://localhost:4000/threads/{email}")
+    res = requests.get(f"{base_url}/threads/{email}")
     return json.loads(res.content)
+
+
+def get_messages(thread_id):
+    st.session_state.selected_thread_id = thread_id
+    res = requests.get(f'{base_url}/messages/{thread_id}')
+
+    st.session_state.messages = json.loads(res.content)
+
+async def add_message(message: dict):
+    st.session_state.messages.append(message)
+    thread_id = st.session_state.selected_thread_id
+    body = {
+        "content": message['content'],
+        "role": message['role'],
+        "thread_id": thread_id
+    }
+    res = requests.post(f'{base_url}/message', json=body)
+    return res.status_code
+
+def get_ai_answer(prompt):
+    body = {
+        "content": prompt,
+        "role": "",
+        "thread_id": ""
+    }
+    res = requests.post(f'{base_url}/chat', json=body)
+    answer = json.loads(res.content)['text'].split()
+
+    for word in answer:
+        yield word + " "
+        time.sleep(0.05)
 
 
 def create_thread(chat_name):
@@ -16,12 +57,11 @@ def create_thread(chat_name):
         "name": chat_name,
         "userId": email
     }
-    res = requests.post('http://localhost:4000/thread', json=body)
+    res = requests.post(f'{base_url}/thread', json=body)
     print(res.text)
-    # Aktualisieren der Threads nach dem Erstellen eines neuen Threads
     return asyncio.run(get_threads())
 
-# Laden der Benutzer
+
 users = asyncio.run(get_users())
 emails = []
 usernames = []
@@ -32,15 +72,12 @@ for user in users:
     usernames.append(user['username'])
     passwords.append(user['password'])
 
-# Erstellen der Authentifizierungs-Credentials
 credentials = {'usernames': {}}
 for index in range(len(emails)):
     credentials['usernames'][usernames[index]] = {'name': emails[index], 'password': passwords[index]}
 
-# Initialisieren des Authentifikators
 Authenticator = stauth.Authenticate(credentials, cookie_name='Streamlit', key="abcd", cookie_expiry_days=5)
 
-# Login-Authentifizierung
 email, authentication_status, username = Authenticator.login(':green[Login]', 'main')
 
 info, info1 = st.columns(2)
@@ -48,21 +85,43 @@ info, info1 = st.columns(2)
 if not authentication_status:
     sign_up()
 elif authentication_status:
-    # Threads abrufen und anzeigen
     threads = asyncio.run(get_threads())
-    st.success("logged in")
+    if len(st.session_state.messages) == 0:
+        st.success("Chat with me!")
 
+    # Sidebar
     with st.sidebar:
-        Authenticator.logout("Logout")
+        Authenticator.logout(":red[Logout]")
+        st.subheader("Chats")
         for thread in threads:
-            st.button(label=thread["name"], key=f"thread-{thread['id']}")
+            if st.button(label=thread["name"], use_container_width=True):
+                get_messages(thread["id"])
+                st.rerun()
 
-        chat_name = st.text_input(label="Chat name", placeholder="Enter a Chat name")
+        chat_name = st.text_input(label="Create a new Chat", placeholder="Enter a Chat name")
 
-        # Überprüfen, ob der Button geklickt wurde, und dann den Thread erstellen
         if st.button(label="create new Chat"):
             if chat_name:
                 threads = create_thread(chat_name)
-                st.rerun()  # Um die Seite zu aktualisieren und die neuen Threads anzuzeigen
+                st.rerun()
             else:
                 st.error("Please enter a chat name.")
+
+    # Chat Logic: https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    if prompt := st.chat_input("What is up?"):
+        status = asyncio.run(add_message({"role": "user", "content": prompt}))
+        if status != 200:
+            st.warning("Could not save message")
+        with st.chat_message("user"):
+            st.markdown(prompt)
+        with st.chat_message("assistant"):
+            message_placeholder = st.empty()
+            with st.spinner("Thinking..."):
+                full_response = st.write_stream(get_ai_answer(prompt))
+        status = asyncio.run(add_message({"role": "assistant", "content": full_response}))
+        if status != 200:
+            st.warning("Could not save message")
